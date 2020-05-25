64798f1e61a15639e098d7b3d5f2a0768200b552
ricagraca <ricagraca.student@gmail.com>
2020-05-25 22:56:36 +0100

    Added fibonacci program for specific inputs

---
+++ examples/fibonacci.v
@@ -0,0 +1,66 @@
+Require Import Coq.Init.Byte Coq.Strings.Byte.
+From CHIP8 Require Import InstructionSet.
+From CHIP8 Require Import HelperDataTypes.
+From CHIP8 Require Import MainMemory.
+From CHIP8 Require Import MainSystem.
+
+Require Import Coq.Lists.List.
+Require Import Coq.micromega.Lia.
+
+Import InstructionSetNotation.
+
+Fixpoint fibonacci_aux a b c :=
+  match c with
+  | 0 => a
+  | S c' => fibonacci_aux (b) (a+b) c'
+  end.
+
+Fixpoint fibonacci n :=
+  fibonacci_aux 0 1 n.
+
+Definition fibonacci_n_chip8 n :=
+  ( ( v1 := n0 ::: n0 ) ;; (*value of a : n0*)
+    ( v2 := n0 ::: n1 ) ;; (*value of b : n2*)
+    ( v3 := n0 ::: n ) ;; (*value to reach : n4*)
+    ( v4 := n0 ::: n0 ) ;; (*value of counter : n6*)
+    (* Start of program *)
+    ( skip if v4 equal to v3 ) (*if equal go to end : n8*) ;;
+    ( jump to n2 ::: n0 ::: ne ) (* jump to procedure : na*) ;;
+    ( ret ) (* return point : nc *) ;;
+    ( v4 :=+ n0 ::: n1 ) ;;
+    ( v5 := v2 ) ;;
+    ( v2 :=+ v1 ) ;;
+    ( v1 := v5) ;; 
+    ( jump to n2 ::: n0 ::: n8 ) 
+  ).
+
+Definition CHIP_prog prog :=
+  updateRam (prog 512 init_memory) CHIP8InitialStateEmptyRam.
+
+Example sum_to_10_prog : forall n,
+    fibonacci (n_to_nat n) < 256 -> 
+    Some (nth 1 (exec_step (CHIP_prog (fibonacci_n_chip8 n)) 2000).(registers) x00)
+    =
+    of_nat (fibonacci (n_to_nat n)).
+Proof.
+  intros n. intro H ; destruct n eqn:E.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + simpl in H. lia.
+  + simpl in H. lia.
+Qed.
+
+
+
--- examples/sum_to_10.v
+++ examples/sum_to_10.v
@@ -11,11 +11,11 @@ Import InstructionSetNotation.
 Check CHIP8InitialStateEmptyRam.
 
 Definition sum_to_10 :=
-  ( ( v1 := n0 ::: n1 ) ;; (*counter : n0*)
+  ( ( v1 := n0 ::: n0 ) ;; (*counter : n0*)
     ( v2 := n0 ::: na ) ;; (*sum to 10 : n2*)
     ( v3 := n0 ::: n0 ) ;; (*sum : n4*)
-    ( v3 :=+ v1 ) ;; (*add v1 to v3 : n6*)
     ( v1 :=+ n0 ::: n1 ) ;; (*update counter*)
+    ( v3 :=+ v1 ) ;; (*add v1 to v3 : n6*)
     ( skip if v1 equal to v2 ) ;;
     ( jump to n2 ::: n0 ::: n6 ) ;;
     ( ret ) (*exit?*)
@@ -24,10 +24,8 @@ Definition sum_to_10 :=
 Definition CHIP_prog prog :=
   updateRam (prog 512 init_memory) CHIP8InitialStateEmptyRam.
 
-Compute (nth 1 (exec_step (CHIP_prog sum_to_10) 14).(registers) x00).
-
 Example sum_to_10_prog :
-      (nth 1 (exec_step (CHIP_prog sum_to_10) 14).(registers) x00) = x04.
+      (nth 3 (exec_step (CHIP_prog sum_to_10) 41).(registers) x00) = x37.
 Proof.
   reflexivity.
 Qed.
