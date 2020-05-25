b9abd8026bbde2375f1e33b30babec446d47fd88
ricagraca <ricagraca.student@gmail.com>
2020-05-25 17:36:32 +0100

    write_instruction and write_all_instructions

--- theories/MainMemory.v
+++ theories/MainMemory.v
@@ -22,3 +22,17 @@ Fixpoint write_memory {A : Type}(data : A) (address : nat) (ram : list A) : list
     |S address', head :: tail => head :: write_memory data address' tail
   end.
   
+Definition write_instruction (inst : byte * byte) (address : nat) (ram : list byte) : list byte :=
+  match inst with
+  | (b1, b2) =>
+    write_memory b2 (S(address)) (write_memory b1 address ram)
+  end.
+
+Fixpoint write_all_instructions (insts : list (byte * byte)) (address : nat) (ram : list byte) : list byte :=
+  match insts with
+  | []              => ram
+  | inst::insts' =>
+    let new_ram := write_instruction inst address ram in
+    write_all_instructions insts' (S(S(address))) new_ram
+  end.
+
