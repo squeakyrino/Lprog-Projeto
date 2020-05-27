94d1eb5e25aaab7fe0d7f3b196d3c701d9b72cb2
ricagraca <ricagraca.student@gmail.com>
2020-05-27 02:08:52 +0100

    fib program for every step

--- examples/fibonacci.v
+++ examples/fibonacci.v
@@ -7,6 +7,7 @@ From CHIP8 Require Import MainSystem.
 Require Import Coq.Numbers.Natural.Peano.NPeano.
 Require Import Coq.Lists.List.
 Require Import Coq.micromega.Lia.
+Require Import Coq.omega.Omega.
 
 Import InstructionSetNotation.
 
@@ -38,19 +39,75 @@ Definition fibonacci_n_chip8 n :=
 Definition CHIP_prog prog :=
   updateRam (prog 512 init_memory) CHIP8InitialStateEmptyRam.
 
-Example fib_prog : forall n s, exists s',
+Example fib_prog0 : forall s, exists s',
+    s > s' ->
+    Some (nth 1 (exec_step (CHIP_prog (fibonacci_n_chip8 n0)) s).(registers) x00)
+    =
+    of_nat (modulo (fibonacci (n_to_nat n0)) 256).
+Proof.
+  intros s.
+  exists 4. intros.
+  induction H.
+  + reflexivity.
+  + do 5 (destruct m ; try lia).
+    assumption.    
+Qed.
+
+Example fib_prog1 : forall s, exists s',
+    s > s' ->
+    Some (nth 1 (exec_step (CHIP_prog (fibonacci_n_chip8 n1)) s).(registers) x00)
+    =
+    of_nat (modulo (fibonacci (n_to_nat n1)) 256).
+Proof.
+  intros s.
+  exists 12. intros.
+  induction H.
+  + reflexivity.
+  + do 13 (destruct m ; try omega).
+    assumption.    
+Qed.
+
+Example fib_prog2 : forall s n, exists s',
     s > s' ->
     Some (nth 1 (exec_step (CHIP_prog (fibonacci_n_chip8 n)) s).(registers) x00)
     =
     of_nat (modulo (fibonacci (n_to_nat n)) 256).
 Proof.
-  intros n s.
-  exists 172. intros.
+  intros s n.
+  exists (4 + 8* (n_to_nat n)). intros.
   induction H.
-  + destruct n eqn:E ; reflexivity.
-  + simpl. simpl in IHle.
-    do 173 (destruct m ; try lia).
+  + destruct n ; reflexivity.
+  + destruct n eqn: E; simpl in H.                        
+    - do 5 (destruct m ; try omega).
+      assumption.    
+    - do 13 (destruct m ; try omega).
+      assumption. 
+    - do 21 (destruct m ; try omega).
+      assumption.
+    - do 29 (destruct m ; try omega).
+      assumption. 
+    - do 37 (destruct m ; try omega).
+      assumption. 
+    - do 45 (destruct m ; try omega).
+      assumption. 
+    - do 53 (destruct m ; try omega).
+      assumption. 
+    - do 61 (destruct m ; try omega).
+      assumption. 
+    - do 69 (destruct m ; try omega).
+      assumption. 
+    - do 77 (destruct m ; try omega).
+      assumption. 
+    - do 85 (destruct m ; try omega).
+      assumption. 
+    - do 93 (destruct m ; try omega).
+      assumption. 
+    - do 101 (destruct m ; try omega).
+      assumption. 
+    - do 109 (destruct m ; try omega).
+      assumption. 
+    - do 117 (destruct m ; try omega).
+      assumption. 
+    - do 125 (destruct m ; try omega).
+      assumption. 
 Qed.
-
-
-
